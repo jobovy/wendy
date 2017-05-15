7a16737b5e10edfcb6be81d7f9aeba411916e233
Jo Bovy <bovy@astro.utoronto.ca>
2017-05-15 00:41:39 -0400

    Don't cover some lines

--- wendy/wendy.py
+++ wendy/wendy.py
@@ -12,7 +12,7 @@ from numpy.ctypeslib import ndpointer
 _lib= None
 outerr= None
 PY3= sys.version > '3'
-if PY3:
+if PY3: # pragma: no cover
     _ext_suffix= sysconfig.get_config_var('EXT_SUFFIX')
 else:
     _ext_suffix= '.so'
@@ -20,7 +20,7 @@ for path in sys.path:
     try:
         _lib = ctypes.CDLL(os.path.join(path,'wendy_c%s' % _ext_suffix))
     except OSError as e:
-        if os.path.exists(os.path.join(path,'wendy_c%s' % _ext_suffix)):
+        if os.path.exists(os.path.join(path,'wendy_c%s' % _ext_suffix)):# pragma: no cover
             outerr= e
         _lib = None
     else:
