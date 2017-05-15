3a6904454e80594e18d15a85d273ddc88829b338
Jo Bovy <bovy@astro.utoronto.ca>
2017-05-14 23:38:43 -0400

    Add automatic tests with travis

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
+import sys
 from setuptools import setup
 from distutils.core import Extension
     
+#Option to track coverage
+try:
+    coverage_pos = sys.argv.index('--coverage')
+except ValueError:
+    extra_compile_args= []
+    extra_link_args= []
+else:
+    del sys.argv[coverage_pos]
+    extra_compile_args= ["-O0","--coverage"]
+    extra_link_args= ["--coverage"]
+
 setup(name='wendy',
       version='0.1',
       description='One-dimensional gravitational N-body code',
@@ -14,6 +26,8 @@ setup(name='wendy',
       include_package_data=True,
       install_requires=['numpy>=1.7'],
       ext_modules=[Extension('wendy_c',
-                       sources=['wendy/wendy.c','wendy/bst.c'],
-                       libraries=['m'],
-                       include_dirs=['wendy/'])])
+                             sources=['wendy/wendy.c','wendy/bst.c'],
+                             libraries=['m'],
+                             include_dirs=['wendy/'],
+                             extra_compile_args=extra_compile_args,
+                             extra_link_args=extra_link_args)])
