a0dfce6274629ab13372691746cf20662af2724c
Jo Bovy <bovy@astro.utoronto.ca>
2020-11-10 21:19:05 -0500

    LCOV_EXCL_... on its own line?

--- wendy/bst.c
+++ wendy/bst.c
@@ -80,7 +80,8 @@ struct node * bst_minValueNode(struct node * node){
     current = current->left;
   return current;
 }
-// Function to print the tree in order GCOV_EXCL_START
+// Function to print the tree in order
+// LCOV_EXCL_START
 void bst_inorder(struct node *root){
   if (root != NULL){
     bst_inorder(root->left);
@@ -99,4 +100,4 @@ int bst_nNode(struct node *root){
   }
   return out;
 }
-// GCOV_EXCL_STOP
+// LCOV_EXCL_STOP
